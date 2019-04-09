767c9e89fc5b77c6a6e4a756d3157dce3c3788be
kaiser <kaisersunhk@gmail.com>
2019-04-09 00:22:43 -0700

    app4

---
+++ 474_lab1/app4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+//main
+
+int main(void)
+{
+     int time1 = 0, time2 = 0;
+     printf("Please enter two numbers:");
+     scanf("%d%d", &time1, &time2);
+     for(;;) {
+        if(time1 <= time2){
+        printf("A C\n");
+        sleep(time1);
+        printf("A B C D\n");
+        sleep(time2-time1);
+      } else {
+        printf("A C\n");
+        sleep(time1);
+        printf("A B C D\n");
+        sleep(time1-time2);
+       }
+   
+
+      }
+    return 0;
+}
