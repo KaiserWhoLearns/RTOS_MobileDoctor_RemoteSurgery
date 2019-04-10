e1791a7669677cca1c7b96dd552152c9e6044be2
kaiser <kaisersunhk@gmail.com>
2019-04-09 17:50:53 -0700

    app3

--- 474_lab1/app3.c
+++ 474_lab1/app3.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 //main
 
 int main(void)
 {
      while(1) {
+        system("clear");
         printf("A C\n");
         sleep(1);
+        system("clear");
         printf("A B C D\n");
         sleep(1);
       }
