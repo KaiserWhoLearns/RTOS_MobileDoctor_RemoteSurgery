492fdce4c34aa9c384832bca5d116445844c6f64
kaiser <kaisersunhk@gmail.com>
2019-04-09 19:13:51 -0700

    app4 fixed

--- 474_lab1/app4.c
+++ 474_lab1/app4.c
@@ -5,12 +5,12 @@
 
 int main(void)
 {
-     int time1 = 0, time2 = 0;
-     printf("Please enter two numbers:");
+    int time1 = 0, time2 = 0;
+    printf("Please enter two numbers:");
      scanf("%d%d", &time1, &time2);
     //assume time1 <= time2
 
-  if(time1 != time2) {
+  if(time2 % time1 != 0) {
     //specifiy that time1 != time2
    while(1) {
       for (int i = 0; i < time2-time1-1; i ++) {
@@ -24,17 +24,31 @@ int main(void)
       system("clear");
       printf("C D\n");
       sleep(1);
-
     }
-  } else {
-    //When time1 = time2
+  } else if(time1 == time2){
     while(1) {
+    //When time1 = time2
         printf("%s","A B C D\n");
         // must need new line character; because the string is first print to a buffer.
         sleep(1);
         system("clear");
         sleep(time1);
     }
-  }
+      } else {
+        while(1) {
+        int i = 1;
+        while(time1 * i != time2) {
+          system("clear");
+          sleep(time1 - 1);
+          printf("%s", "A B\n");
+          sleep(1);
+          i++;
+        }
+        system("clear");
+        printf("A B C D\n");
+        sleep(1);
+        system("clear");
+      }
+    }
     return 0;
 }
--- 474_lab1/app5.c
+++ 474_lab1/app5.c
@@ -1,17 +1,36 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
+void f1Data(unsigned long delay1);
+void f2Clear(unsigned long delay2);
 
-void main(){
+int main(){
+	int time1 = 0, time2 = 0;
+    printf("Please enter two numbers:");
+    scanf("%d%d", &time1, &time2);
+	// assume time1 > time2
 
-
+    while(1) {
+		if(time2 % time1 != 0) {
+		system("clear");
+        f1Data(time1);
+		f2Clear(time2-time1);
+		printf("C D\n");
+		sleep(1);
+		}
+	}
 	return 0;
 }
 
+// the function that display and flash A B
 void f1Data(unsigned long delay1) {
-
+    sleep(delay1 - 1);
+	printf("A B\n");
+	sleep(1);
 }
 
+// the function that display and flash C D
 void f2Clear(unsigned long delay2) {
-
+	system("clear");
+	sleep(delay2 - 1);
 }
