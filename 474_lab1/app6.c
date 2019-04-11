829f34787585e4b991c09abcf0eaf74a43cbb3d0
kaiser <kaisersunhk@gmail.com>
2019-04-11 13:12:17 -0700

    small changes, app5, 6, 7 nedded

--- 474_lab1/app6.c
+++ 474_lab1/app6.c
@@ -2,6 +2,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+void f1Data(unsigined long delay1);
 int main(void) {
+    
+}
+void f1Data(unsigined long delay1){
+
+}
+void f2Clear(unsigined long delay2) {
     
 }
