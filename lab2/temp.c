7f5fd2d26f140902fd877e562c094d024832aa9d
kaiser <kaisersunhk@gmail.com>
2019-04-26 10:22:23 -0700

    branch added

--- lab2/temp.c
+++ lab2/temp.c
@@ -13,19 +13,25 @@ void intToChar(char* arr, int val);
 
 
 int main() {
-    tempStruct temp = {tryPtr};
-    tempStruct* tempPtr = &temp;
-    tempStruct anotherTemp = *tempPtr;
-    int t = *(anotherTemp.tryPtr);
-    printf("%d\n", t);
-    *(anotherTemp.tryPtr) += 2;
-    t = *(anotherTemp.tryPtr);
-    printf("%d\n", t);
-    printf("%d\n", *(temp.tryPtr));
-    char tem[3];
-    // tem = String(123);
-    intToChar(tem, 12);
-    printf("%s", tem);
+    // tempStruct temp = {tryPtr};
+    // tempStruct* tempPtr = &temp;
+    // tempStruct anotherTemp = *tempPtr;
+    // int t = *(anotherTemp.tryPtr);
+    // printf("%d\n", t);
+    // *(anotherTemp.tryPtr) += 2;
+    // t = *(anotherTemp.tryPtr);
+    // printf("%d\n", t);
+    // printf("%d\n", *(temp.tryPtr));
+    // char tem[3];
+    // // tem = String(123);
+    // intToChar(tem, 12);
+    // printf("%s", tem);
+    int temp = 10;
+    int temp2 = 4;
+    int* tempPtr = &temp;
+    printf("%d", temp);
+    *(tempPtr) = temp2;
+    printf("%d", temp);
     return 0;    
 }
 
